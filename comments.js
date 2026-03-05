9cdc89be7d718fe32de00cb82a12541b9c92b4bb
Akhmedov Isa <akhmedovisa167@gmail.com>
2026-03-05 20:52:37 +0500

    fix mistakes

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-export const infoForUsers = [
+export const comments = [
   {
     postId: 1,
     id: 1,
--- homework-7.js
+++ homework-7.js
@@ -1,7 +1,9 @@
+import { comments } from "./comments.js";
+
 // №2
 
 const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newArrayNumbers = arrayNumbers.filter(num => num >= 5);
+const filteredNumbers = arrayNumbers.filter(num => num >= 5);
 
 // №3
 
@@ -12,22 +14,20 @@ console.log(checkSimilar);
 
 // №4
 
-const reverseArray = (array) => (console.log(array.reverse()));
+const reverseArray = (array) => (array.reverse());
 
 reverseArray(films);
-reverseArray(newArrayNumbers);
+reverseArray(filteredNumbers);
 
 // №5,6,7
 
-import { infoForUsers } from "./comments.js";
-
-const onlyEmailComAUsers = infoForUsers.filter((user) => user.email.includes(".com"));
+const onlyEmailComAUsers = comments.filter((user) => user.email.includes(".com"));
 
 console.log(onlyEmailComAUsers);
 
 // №8
 
-const newArrayUsers = infoForUsers.map((user) => {
+const newArrayUsers = comments.map((user) => {
   return { ...user, postId: user.id <= 5 ? 2 : 1};
 });
 
@@ -35,13 +35,13 @@ console.log(newArrayUsers);
 
 // №9 посложнее вариант
 
-const condensedInfoForUsers = infoForUsers.map(({ id, name }) => ({ id, name }));
+const condensedInfoForUsers = comments.map(({ id, name }) => ({ id, name }));
 
 console.log(condensedInfoForUsers);
 
 // №9 попроще вариант
 
-const condensedInfoForUsers2 = infoForUsers.map(user => {
+const condensedInfoForUsers2 = comments.map(user => {
   return { id: user.id, name: user.name };
 });
 
@@ -49,27 +49,27 @@ console.log(condensedInfoForUsers2);
 
 // №10
 
-infoForUsers.forEach(user => {
-  user.isInvalid = user.body > 180 ? "true" : "false";
-})
+comments.forEach(user => {
+  user.isInvalid = user.body > 180;
+});
 
-console.log(infoForUsers);
+console.log(comments);
 
 // №11 через reduce
 
-const arrayEmails = infoForUsers.reduce((array, { email }) => {
-  array.push(email)
+const arrayEmailsUsingReduce = comments.reduce((array, { email }) => {
+  array.push(email);
   return array;
-},[]);
+}, []);
 
-console.log(arrayEmails);
+console.log(arrayEmailsUsingReduce);
 
 // №11 через map
 
-const arrayEmails2 = infoForUsers.map(user => user.email)
+const arrayEmailsUsingMap = comments.map(user => user.email)
 
-console.log(arrayEmails2);
+console.log(arrayEmailsUsingMap);
 
 // №12
 
-console.log(arrayEmails.join(', '));
+console.log(arrayEmailsUsingMap.join(', '));
